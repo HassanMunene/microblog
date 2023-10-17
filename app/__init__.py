a0298ff1f91ea83601d3717b0620fe674aedc448
HassanMunene <awanzihassan@gmail.com>
2023-10-17 10:26:38 +0300

    I have added bootstrap to the application

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The module that will initialzie this app package
-"""
-from flask import Flask
-
-app = Flask(__name__)
-
-from app import route
--- microblog.py
+++ microblog.py
@@ -1,1 +1,30 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'title': 'Blog post 1',
+        'author': 'Hassan Munene',
+        'date': 'Oct 2023 17',
+        'content': 'First blog post'
+    },
+    {
+        'title': 'Blog post 2',
+        'author': 'Munene Awanzi',
+        'date': 'Oct 2023 16',
+        'content': 'pre first blog post'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
