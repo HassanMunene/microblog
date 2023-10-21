1f9ad0a1000b2e553001be2ab2abc44f2d0549b9
HassanMunene <awanzihassan@gmail.com>
2023-10-22 00:44:42 +0300

    have done and tryied alot

---
+++ app/static/assets/js/style.js
@@ -0,0 +1,12 @@
+/*
+ * This is an ajax query by jquery to retrieve informationn from the server
+ *
+ */
+
+/*$(document).ready(function () {
+    $('#getStartedLink').click(function () {
+        $.get("/register", function(data) {
+            $("#registration-modal-content").html(data);
+        });
+    });
+});*/
