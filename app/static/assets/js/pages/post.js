8394d667eeede1fe97d0ba4fd7cab4a1fe6b8c29
HassanMunene <awanzihassan@gmail.com>
2023-11-26 22:16:36 +0300

    more javascript

--- app/static/assets/js/pages/post.js
+++ app/static/assets/js/pages/post.js
@@ -3,11 +3,36 @@
 * ==========================================================================*/
 document.addEventListener('DOMContentLoaded', function () {
     const postImageContainer = document.getElementById('postImageContainer');
-    const postImage = document.getElementById('postImage');
     if (postImageContainer) {
         postImageContainer.addEventListener('click', function () {
-            postImageContainer.classList.add('show');
-            postImage.classList.add('show');
+            setTimeout(function () {
+                postImageContainer.classList.toggle('show');
+            }, 500)
         })
     }
 })
+
+/*===============================================================================
+* ============Manipulate the post content in the post page ======================
+* =============================================================================*/
+document.addEventListener("DOMContentLoaded", function () {
+    // Assuming you have an element with the id 'actual-post'
+    let postContent = document.getElementById('postBody');
+
+    let postText = postContent.textContent;
+    let firstWord = postText.split(" ")[0];
+    console.log(firstWord);
+
+    //make sure firstWord is not an empty string
+    if (firstWord.trim() !== '') {
+        // create a span element to wrap the firstWord in it
+        let firstWordSpan = document.createElement('span');
+        firstWordSpan.className = 'firstWord';
+        firstWordSpan.textContent = firstWord;
+        postContent.innerHTML = postText.replace (
+            new RegExp(firstWord, 'i'),
+            firstWordSpan.outerHTML
+        );
+        console.log(postContent.innerHTML);
+    }
+});
