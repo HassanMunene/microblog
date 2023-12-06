82cbe43a040fd4501c13d6d0ee0eb890d93c8b84
HassanMunene <awanzihassan@gmail.com>
2023-12-06 18:36:57 +0300

    a really big milestone

--- app/static/assets/js/pages/home.js
+++ app/static/assets/js/pages/home.js
@@ -29,34 +29,6 @@
 })*/
 
 /*==================================================================================
-* ==When the getStartedButton is clicked show the sign up modal=====================
-* ==============================================================================*/
-
-document.addEventListener('DOMContentLoaded', function () {
-    let getStartedButtons = document.getElementsByClassName('getStartedButton');
-    const body = document.body;
-
-    if (getStartedButtons) {
-        getStartedButtons = [...getStartedButtons];
-        const closeModalButton = document.getElementById('closeButton');
-        const getStartedModal = document.getElementById('getStartedModal');
-
-        function showGetStartedModal() {
-            body.classList.add('modal-open');
-            getStartedModal.classList.add('show');
-        }
-        function closeGetStartedModal() {
-            body.classList.remove('modal-open');
-            getStartedModal.classList.remove('show');
-        }
-        getStartedButtons.forEach(function(getStartedButton) {
-            getStartedButton.addEventListener('click', showGetStartedModal);
-        })
-        closeModalButton.addEventListener('click', closeGetStartedModal);
-    }
-})
-
-/*==================================================================================
 * ===This section we are changing background color of getStartedButton  and the navbar============== 
 * ===depending on the height of the scrolly and section=============================
 * ================================================================================*/
@@ -114,46 +86,115 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
+
+/*==================================================================================
+* ==When the getStartedButton is clicked show the sign up modal=====================
+* ==============================================================================*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    let getStartedButtons = document.getElementsByClassName('getStartedButton');
+    const body = document.body;
+
+    if (getStartedButtons) {
+        getStartedButtons = [...getStartedButtons];
+        const closeModalButton = document.getElementById('closeButton');
+        const getStartedModal = document.getElementById('getStartedModal');
+
+        function showGetStartedModal() {
+            body.classList.add('modal-open');
+            getStartedModal.classList.add('show');
+        }
+        function closeGetStartedModal() {
+            body.classList.remove('modal-open');
+            getStartedModal.classList.remove('show');
+        }
+        getStartedButtons.forEach(function(getStartedButton) {
+            getStartedButton.addEventListener('click', showGetStartedModal);
+        })
+        closeModalButton.addEventListener('click', closeGetStartedModal);
+
+        //When user clicks the signup with email button in in getstarted modal show sign up modal
+        const emailSignUpButton  = document.getElementById('email_signup');
+        const signUpModal = document.getElementById('signupModal');
+
+
+        // The following functions are for the signUpModal
+        function closeSignUpModal () {
+            body.classList.remove('modal-open');
+            signUpModal.classList.remove('show');
+        }
+        // here we will show the the signUp Modal and handle everything in that Modal
+        function showEmailSignUpModal () {
+            closeGetStartedModal();
+            body.classList.add('modal-open');
+            signUpModal.classList.add('show');
+
+            const closeSignUpButton = document.getElementById('closeSignUpButton');
+            if (signUpModal) {
+                const welcomeElement = document.getElementById('welcomeMessage');
+                const formContainer = document.getElementById('form-container');
+            
+                // the message that will be dynamically inserted into the element
+                const welcomeText = "Welcome to KcaVibes! Let's begin the adventure!";
+                // the string can be accessed like an array so we declare an iterator
+                let counter = 0;
+        
+                let intervalId = setInterval(function (){
+                    let nextLetter = welcomeText[counter];
+                    welcomeElement.innerHTML += nextLetter;
+                    counter++;
+            
+                    if (counter === welcomeText.length) {
+                        setTimeout(function (){
+                            formContainer.style.display = 'block';
+                        }, 1000)
+                        clearInterval(intervalId);
+                    }
+               }, 100)
+        
+               // when user enters the email and clicks continue we should show them the password field
+        
+               const continueToPasswordButton = document.getElementById('continueToPassword');
+               const submitDataButton = document.getElementById('submitDataButton');
+               const passwordContianer = document.getElementById('passwordContainer');
+               let email = document.getElementById('email');
+               let passwordInput = document.getElementById('password');
+               let showPassword = document.getElementById('showPassword');
+               let hidePassword = document.getElementById('hidePassword');
+        
+               continueToPasswordButton.addEventListener('click', function () {
+                //get the input details of the email input
+                email = document.getElementById('email').value;
+                if (email !== '') {
+                    passwordContianer.style.display = 'block';
+                }
+               })
+               
+               // when user clicks show password button show the password in text format
+               showPassword.addEventListener('click', function () {
+                passwordInput.type = 'text';
+                showPassword.style.display = 'none';
+                hidePassword.style.display = 'block';
+               })
+               // when user clicks hide password button show password in password format
+               hidePassword.addEventListener('click', function () {
+                passwordInput.type = 'password';
+                hidePassword.style.display = 'none';
+                showPassword.style.display = 'block';
+               })
+               //when submit data button is clicked, send data to the backend using fetch api
+               submitDataButton.addEventListener('click', function () {
+                    console.log(email.value, passwordInput.value);
+               })
+               closeSignUpButton.addEventListener('click', closeSignUpModal);
+            }
+        }
+
+        emailSignUpButton.addEventListener('click', showEmailSignUpModal);
+    }
+
+})
+
 /*==================================================================================
 * ===Show a welcome message when user wants to sign up dynamically using setInterval========
 * ================================================================================*/
-document.addEventListener('DOMContentLoaded', function () {
-    
-    const signUpModal = document.getElementById('signupModal');
-    if (signUpModal) {
-        const welcomeElement = document.getElementById('welcomeMessage');
-        const formContainer = document.getElementById('form-container');
-    
-        // the message that will be dynamically inserted into the element
-        const welcomeText = "Welcome to KcaVibes! Let's begin the adventure!";
-        // the string can be accessed like an array so we declare an iterator
-        let counter = 0;
-
-        let intervalId = setInterval(function (){
-            let nextLetter = welcomeText[counter];
-            welcomeElement.innerHTML += nextLetter;
-            counter++;
-    
-            if (counter === welcomeText.length) {
-                setTimeout(function (){
-                    formContainer.style.display = 'block';
-                }, 1000)
-                clearInterval(intervalId);
-            }
-       }, 100)
-
-       // when user enters the email and clicks continue we should show them the password field
-
-       const continueToPassword = document.getElementById('continueToPassword');
-       const passwordContianer = document.getElementById('passwordContainer');
-
-       continueToPassword.addEventListener('click', function () {
-        //get the input details of the email input
-        let email = document.getElementById('email').value;
-        console.log(email)
-        if (email !== '') {
-            passwordContianer.style.display = 'block';
-        }
-       })
-    }
-})
